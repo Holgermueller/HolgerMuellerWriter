ba20e488da68809feab99984822ec426a2d3e89f
Holger Mueller <9jholger@gmail.com>
2021-09-10 11:18:12 -0500

    began add comments dialog

---
+++ src/components/SingleBookComp/AddComments.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <v-dialog>
+      <template>
+        <v-btn> </v-btn>
+      </template>
+    </v-dialog>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AddCommentDialog",
+
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style scoped></style>
