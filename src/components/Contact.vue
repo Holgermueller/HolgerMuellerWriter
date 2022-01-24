2ed0d6d038374ef055baba98f9cc07d36fe8c0f3
Holger Mueller <9jholger@gmail.com>
2022-01-24 11:54:02 -0600

    worked on form validation

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -1,16 +1,46 @@
 <template>
   <div id="Contact">
+    <h3 class="page-header">Drop me a line:</h3>
     <v-form ref="form">
-      <v-text-field label="Name" outlined clearable></v-text-field>
-      <v-text-field label="Email" outlined clearable></v-text-field>
-      <v-textarea label="Message..." outlined clearable></v-textarea>
+      <v-text-field
+        v-model="name"
+        :rules="nameRules"
+        required
+        label="Name"
+        outlined
+        clearable
+      ></v-text-field>
+      <v-text-field
+        v-model="email"
+        :rules="emailRules"
+        required
+        label="E-mail"
+        outlined
+        clearable
+      ></v-text-field>
+      <v-textarea
+        v-model="message"
+        :rules="messageRules"
+        required
+        label="Message..."
+        outlined
+        clearable
+        counter="140"
+      ></v-textarea>
 
       <v-btn color="success" elevation="0" dark block @click="validate"
         >Submit</v-btn
       >
     </v-form>
 
-    <div id="SocialMedia" class="socials"></div>
+    <div id="SocialMedia" class="socials">
+      <v-btn x-large icon>
+        <v-icon>mdi-twitter</v-icon>
+      </v-btn>
+      <v-btn x-large icon>
+        <v-icon>mdi-instagram</v-icon>
+      </v-btn>
+    </div>
   </div>
 </template>
 
@@ -18,10 +48,37 @@
 export default {
   name: "ContactPage",
 
-  data: () => ({}),
+  data: () => ({
+    valid: false,
+    name: "",
+    email: "",
+    message: "",
+    nameRules: [
+      (v) => !!v || "Name is required",
+      (v) => v.length <= 10 || "Name must be less than ten characters",
+    ],
+    emailRules: [
+      (v) => !!v || "E-mail is required",
+      (v) => /.+@.+/.test(v) || "E-mail must be valid",
+    ],
+    messageRules: [
+      (v) => !!v || "Message is required",
+      (v) => v.length <= 140 || "Message must be less than 140 characters",
+    ],
+  }),
 
   methods: {
     validate() {},
   },
 };
 </script>
+
+<style scoped>
+.page-header {
+  text-align: left;
+  margin-bottom: 5%;
+}
+.socials {
+  margin: 5% auto;
+}
+</style>
