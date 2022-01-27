e5514e9f2a0b8c6288fca0d988fba5b7fbc495c3
Holger Mueller <9jholger@gmail.com>
2022-01-27 12:02:23 -0600

    set up experience page

--- src/components/Experience.vue
+++ src/components/Experience.vue
@@ -7,14 +7,21 @@
     </div>
 
     <div v-for="job in jobs" :key="job.placeOfEmployment">
-      <div>
-        <h2>
-          {{ job.placeOfEmployment }}
-        </h2>
-        <h3>
-          {{ job.yearsWorked }}
-        </h3>
-      </div>
+      <v-container>
+        <v-row>
+          <v-col>
+            <h2>
+              {{ job.placeOfEmployment }}
+            </h2>
+          </v-col>
+          <v-col class="years-worked">
+            <h3>
+              {{ job.yearsWorked }}
+            </h3>
+          </v-col>
+        </v-row>
+      </v-container>
+      <div></div>
 
       <ul v-for="(duty, i) in job.duties" :key="i">
         <li>
@@ -32,9 +39,47 @@ export default {
   data: () => ({
     jobs: [
       {
-        placeOfEmployment: "blah",
-        yearsWorked: "blah",
-        duties: ["blah", "blah", "blah"],
+        placeOfEmployment: "Freelance Content Writer, Zergnet",
+        yearsWorked: "2015-2016",
+        duties: [
+          "Composed compelling content for Looper and Grunge websites",
+          "Organized article materials for smooth reading experience",
+          "Included appropriate images to accompany articles",
+        ],
+      },
+      {
+        placeOfEmployment:
+          "Adjunct Professor of English, SUNY Orange, Middletown, NY",
+        yearsWorked: "2013-2015",
+        duties: [
+          "Composed writing assignments and exercises on various topics to maximize student engagement with course materials",
+          "Communicated complex, abstract concepts and technical terms to multiple students in clear, understandable terms",
+          "Proofread student papers to point out areas for improvement",
+          "Met with students individually to plan best approaches for improving writing skills",
+        ],
+      },
+      {
+        placeOfEmployment:
+          "Faculty Writing Consultant, SUNY Orange, Middletown, NY",
+        yearsWorked: "2013-2015",
+        duties: [
+          "Communicated directly with students to assess their skills and develop solutions to meet their needs",
+          "Communicated complex, abstract concepts and technical terms  to students in clear, understandable terms",
+          "Composed writing exercises to improve students’ writing skills",
+          "Worked with multiple students based on individual progress and need",
+          "Proofread student papers to point out areas for student improvement",
+        ],
+      },
+      {
+        placeOfEmployment: "Writing Consultant, SUNY Orange, Middletown, NY",
+        yearsWorked: "2019-2015",
+        duties: [
+          "Communicated directly with students to assess their skills and develop solutions to meet their needs",
+          "Communicated complex, abstract concepts and technical terms  to students in clear, understandable terms",
+          "Composed writing exercises to improve students’ writing skills",
+          "Worked with multiple students based on individual progress and need",
+          "Proofread student papers to point out areas for student improvement",
+        ],
       },
     ],
   }),
@@ -47,5 +92,9 @@ export default {
 }
 h1 {
   margin: 8px 0;
+}
+
+.years-worked {
+  text-align: right;
 }
 </style>
